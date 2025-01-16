5c2f74b57a902696f7efacc8f6003574d4f6a280
Debjit Patra <debjitpatra8521@gmail.com>
2025-01-16 16:29:29 +0530

    Added

---
+++ src/script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz7jZnPRWUOZ9tBKA88ehSoByKEQeZVGvkgQW9DiilEIAMNOJYEpOaxtKTKudRigJRE/exec'
+    const form = document.forms['submit-to-google-sheet']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        const submitButton = form.querySelector('button');
+        submitButton.disabled = true;
+        submitButton.textContent = "Sending...";
+
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                alert('Your message has been sent successfully!');
+                form.reset(); // Optionally reset the form after successful submission
+                submitButton.disabled = false;
+                submitButton.textContent = "Send";
+            })
+            .catch(error => {
+                console.error('Error!', error.message);
+                submitButton.disabled = false;
+                submitButton.textContent = "Send";
+            });
+    })
+})
